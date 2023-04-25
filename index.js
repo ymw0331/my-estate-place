61c17766fd1b0e6c7b521ccde7fa52e37e905a8b
Wayne Yong <ymw0331@gmail.com>
2023-04-25 16:54:19 +0800

    update index.js

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.use("/api", adRoutes)
 
 
 // port
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8001;
 
 app.listen( port, () => console.log( `Server is running on port ${ port }` ) );
